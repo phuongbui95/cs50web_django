2fc8376a39969e1be65a91d541586e041d92c54c
PB Github <phuong.buitan95@gmail.com>
2023-03-08 18:33:56 +0700

    Fixed url tag  Django template

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path("", views.index, name="index")
     ,path("<str:name>", views.greet, name="greet")
     ,path("pb", views.pb, name="pb")
     
-}
+]
--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -3,9 +3,8 @@ from django.shortcuts import render
 tasks = ["foo", "bar", "baz"]
 # Create your views here.
 def index(request):
-    return render(request, "tasks/index.html", {
-        "tasks": tasks
-    })
+    context = {"tasks": tasks}
+    return render(request, "tasks/index.html", context)
 
 def add(request):
     return render(request, "tasks/add.html")
